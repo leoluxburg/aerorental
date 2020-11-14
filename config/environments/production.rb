54bf3e4da1309c57e744d205d35d0b9f6f670de7
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2020-11-14 18:03:52 -0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
